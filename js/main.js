cf840310604f103363b31eb29c1832988c604d64
Jan1si <nikita.evgrafov@inbox.ru>
2022-07-20 21:31:25 +0500

    view data in Data Base

---
+++ js/main.js
@@ -0,0 +1,120 @@
+const db = {
+  chats:[
+    'general',
+    'support',
+    'marketing',
+    'thailand',
+    'bali',
+    'poland',
+    'australia',
+    'jobs',
+    'startups',
+    'italy',
+    'freelance'
+  ],
+  users:{
+    1:{
+      name:'Orlando Diggs',
+      avatar:'user1.jpg',
+      status:1
+    },
+    2:{
+      name:'Marie Jensen',
+      avatar:'user2.jpg',
+      status:0
+    },
+    3:{
+      name:'Carmen Velasco',
+      avatar:'user3.jpg',
+      status:0
+    },
+    4:{
+      name:'Alex Lee',
+      avatar:'user4.jpg',
+      status:0
+    },
+    5:{
+      name:'Leo Gill',
+      avatar:'user5.jpg',
+      status:1
+    },
+    6:{
+      name:'Britney Cooper',
+      avatar:'user6.jpg',
+      status:0
+    }
+    
+  }
+};
+
+
+
+const chatsBody = document.querySelector(".chats__body"),
+      usersBody = document.querySelector(".users__body"),
+      chatsTitleList = document.querySelector(".title__chats"),
+      usersTitleList = document.querySelector(".title__users");
+
+  // Чаты
+function showChats() {
+  // Удаление элементов 
+  chatsBody.innerHTML= "";
+  // Заполнение элеметами из бд
+  db.chats.forEach(item => {
+    chatsBody.insertAdjacentHTML('beforeend', `<a href="#" class="chat "># ${item}</a>`);
+  });
+} 
+showChats();
+
+// Пользователи канала
+function showUsers() {
+  // Удаление элеиентов
+  usersBody.querySelector("ul").innerHTML = "";
+
+  // Заполнение элементами
+  for (let i = 1; i<Object.keys(db.users).length+1; i++) {
+
+    if (db.users[i].status === 1) {
+      usersBody.querySelector("ul").insertAdjacentHTML("beforeend", `<li class="user__item">
+      <div class="status status-online"></div>
+      <div class="avatar">
+          <img src="img/users/${db.users[i].avatar}" alt="avatar">
+      </div>
+      <div class="name">${db.users[i].name}</div>
+    </li>`);
+    } else {
+      usersBody.querySelector("ul").insertAdjacentHTML("beforeend", `<li class="user__item">
+      <div class="status"></div>
+      <div class="avatar">
+          <img src="img/users/${db.users[i].avatar}" alt="avatar">
+      </div>
+      <div class="name">${db.users[i].name}</div>
+    </li>`);
+    }
+    
+  }
+
+}
+showUsers();
+
+
+function setNameList(id, name) {
+  if (id === 0) {
+    chatsTitleList.querySelector('.name__list').innerText = Object.keys(db)[id];
+    chatsTitleList.querySelector('.count__list').innerText = name.length;
+  } else if (id === 1 ) {
+    usersTitleList.querySelector(".name__list").innerText = Object.keys(db)[id];
+    usersTitleList.querySelector(".count__list").innerText = Object.keys(name).length;
+  } else {
+    return "error";
+  }
+
+}
+
+
+
+setNameList(0, db.chats);
+setNameList(1, db.users);
+
+
+
+
